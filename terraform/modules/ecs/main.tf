b2fde1db6e0875d0f087172b9781f03323c21fca
shiva <shivakumar.renu@yahoo.com>
2025-10-16 00:42:03 +0530

    changes

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -17,6 +17,25 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_security_group" "ecs_sg" {
+  name        = "${var.name}-ecs-sg"
+  description = "Security group for ECS tasks"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 data "aws_iam_policy_document" "ecs_task_assume" {
   statement {
     actions = ["sts:AssumeRole"]
--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -45,12 +45,31 @@ resource "aws_key_pair" "jenkins_key" {
 resource "aws_security_group" "jenkins_sg" {
   name   = "${var.name}-jenkins-sg"
   vpc_id = var.vpc_id
-    ingress {
-      description = "SSH"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins Web"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
     }
 
     ingress {
